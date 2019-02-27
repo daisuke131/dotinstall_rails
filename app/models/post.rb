b2e6e032f8b3d8732de4c4929f6c154ee9b91445
daisuke <dada.00131@google.com>
2019-02-27 23:29:45 +0900

    終了

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
 class Post < ApplicationRecord
-    has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 3 }
   validates :body, presence: true
 end
